d137382b997a3cbcb57839623a2c73434d59bbef
Samuel <62776404+OrlatoDev@users.noreply.github.com>
2020-06-09 21:49:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var todos = [];
+var marcada = [];
+var marcadaIDs = [];
 var j = 0;
 var y = j - 1;
 if (y === -1){
@@ -9,16 +12,19 @@ function clear(){
 }
 
 function concluida(taskname){
-    // var taskname = "task".concat(y);
-    // console.log(taskname);
     var li = document.getElementById(taskname);
     li.setAttribute("style", "text-decoration: line-through;");
+    if (marcada.indexOf(li.getAttribute("id")) > -1){
+        
+    }
+    else{
+        marcada.push(li.getAttribute("id"));
+        console.log(marcada);
+        localStorage.setItem('markeds', JSON.stringify(marcada));
+    }
 }
 
 function addTask(){
-    var app = document.querySelector("#app");
-    var addTaskCont = document.querySelector("#add-task-container");
-    var listContainer = document.querySelector("#list-container");
     var inputTask = document.querySelector("#input-task").value;
     var listTask = document.querySelector("#list-tasks");
 
@@ -26,16 +32,15 @@ function addTask(){
         alert("You can't add a blank task!")
     }
     else{
-        var elementText = document.createTextNode(inputTask);
+        var elementText = document.createTextNode(inputTask+" ");
         var elementTask = document.createElement("li");
         elementTask.setAttribute("id", "task-"+j);
         
             
         var elementButton = document.createElement("button");
-        elementButton.setAttribute("class", "button-list");
-        // elementButton.setAttribute("onclick", "concluida()")
+        elementButton.setAttribute("class", "button-list")
 
-        var elementText2 = document.createTextNode("Marcar como concluída");
+        var elementText2 = document.createTextNode("Mark as done");
         elementButton.appendChild(elementText2);
         
         elementTask.appendChild(elementText);
@@ -43,14 +48,17 @@ function addTask(){
 
         listTask.appendChild(elementTask);
 
+        todos.push(inputTask);
+
         nameLi = listTask;
 
         elementTask.addEventListener('click', function(ev) {
             if (ev.target.tagName === 'BUTTON') {
-                concluida(ev.target.parentElement.id)
+                concluida(ev.target.parentElement.id);
             }
           }, false);
 
+        save()
         clear();
         j+=1;
     }
@@ -58,8 +66,6 @@ function addTask(){
 }
 
 function clearAll(){
-    var app = document.querySelector("#app");
-    var listContainer = document.querySelector("#list-container");
     var listTask = document.querySelector("#list-tasks");
 
     var li = document.querySelectorAll("li");
@@ -75,6 +81,8 @@ function clearAll(){
             continue;
         }
     }
+    localStorage.clear();
+    todos = [];
     j = 0;
 }
 
@@ -83,4 +91,66 @@ function listener(){
     if (key === 13){
         addTask();
     }
+}
+
+function save(){
+    localStorage.setItem('storage-todos', JSON.stringify(todos));
+}
+
+//load localstorage
+var o = 0;
+
+for (o; JSON.parse(localStorage.getItem("storage-todos")).length;){
+    var inputTask = JSON.parse(localStorage.getItem("storage-todos"))[o];
+    var listTask = document.querySelector("#list-tasks");
+    
+    var elementText = document.createTextNode(inputTask+" ");
+    var elementTask = document.createElement("li");
+    elementTask.setAttribute("id", "task-"+j);
+    elementTask.setAttribute("class", "tracking-in-contract");
+    
+        
+    var elementButton = document.createElement("button");
+    elementButton.setAttribute("class", "button-list");
+    
+    var elementText2 = document.createTextNode("Mark as done");
+    elementButton.appendChild(elementText2);
+    
+    elementTask.appendChild(elementText);
+    elementTask.appendChild(elementButton);
+    
+    listTask.appendChild(elementTask);
+    
+    todos.push(inputTask);
+    
+    nameLi = listTask;
+
+    elementTask.addEventListener('click', function(ev) {
+        if (ev.target.tagName === 'BUTTON') {
+            concluida(ev.target.parentElement.id);
+        }
+        }, false);
+    
+    j+=1;
+    o+=1;
+    if (o >= JSON.parse(localStorage.getItem("storage-todos")).length){
+        break;
+        save();
+    }
+    else{
+        continue;
+    }
+}
+
+// load marked
+
+var i = 0;
+
+for (o; JSON.parse(localStorage.getItem('markeds')).length;){
+    var storageMarked = JSON.parse(localStorage.getItem('markeds'))[i];
+
+    var elementMarked = document.querySelector("#"+storageMarked);
+
+    elementMarked.setAttribute("style", "text-decoration: line-through;");
+    i++;
 }
